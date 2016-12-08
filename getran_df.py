964aac5c329fbe74525449e682ca0297fa831678
Jhonathan Davi <jdavi@insightsecurity.com.br>
2016-12-08 17:26:58 -0200

    Update getran_df.py

--- getran_df.py
+++ getran_df.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Script de consulta simples no banco de dados da Banca de Direção do Detran DF / By Jhonathan Davi A.K.A jh00nbr_ / 
-# Insightl4b - http://lab.insightsecurity.com.br
-
-__author__ =  "Jhonathan Davi"
-__credits__ = ["Jhonathan Davi A.K.A jh00nbr_"]
+# Script de consulta simples no banco de dados da Banca de Direção do Detran DF / By Jhonathan Davi A.K.A jh00nbr 
+# jdavi@insightsecurity.com.br
+# Insightl4b - http://lab.insightsecurity.com.br 
+# Blog: lab.insightsecurity.com.br
+# Github: http://github.com/jh00nbr
+# Twitter @jh00nbr
 
 import requests,sys,re
 
+__author__ =  "Jhonathan Davi A.K.A jh00nbr"
+
+dados = {'nome':'','renach':'','cpf':'','dataNascimento':'','local':'','cfc':'','resultado':'', 'data':'','hora':''}
+
+def get_dados(contents): # Regex das tags XML
+    nome = re.search('<nome>(.*)</nome>',contents).group(0).replace('<nome>','').replace('</nome>','')
+    renach = re.search('<renach>(.*)</renach>',contents).group(0).replace('<renach>','').replace('</renach>','')
+    cpf = re.search('<cpf>(.*)</cpf>',contents).group(0).replace('<cpf>','').replace('</cpf>','')
+    dataNascimento = re.search('<dataNascimento>(.*)</dataNascimento>',contents).group(0).replace('<dataNascimento>','').replace('</dataNascimento>','')
+    local = re.search('<local>(.*)</local>',contents).group(0).replace('<local>','').replace('</local>','')
+    cfc = re.search('<cfc>(.*)</cfc>',contents).group(0).replace('<cfc>','').replace('</cfc>','')
+    resultado = re.search('<resultado>(.*)</resultado>',contents).group(0).replace('<resultado>','').replace('</resultado>','')
+    data = re.search('<data>(.*)</data>',contents).group(0).replace('<data>','').replace('</data>','')
+    hora = re.search('<hora>(.*)</hora>',contents).group(0).replace('<hora>','').replace('</hora>','')
+    dados['nome'],dados['renach'],dados['cpf'],dados['dataNascimento'],dados['local'],dados['cfc'],dados['resultado'],dados['data'],dados['hora'] = nome,renach,cpf,dataNascimento,local,cfc,resultado,data,hora
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-    	cpf = sys.argv[1]
-    	url = "http://getran.detran.df.gov.br/getranServicos/ConsultaHabilitacaoWS?wsdl"
+    if len(sys.argv) > 1:          
+        configs = {'api_getran':'http://getran.detran.df.gov.br/getranServicos/ConsultaHabilitacaoWS?wsdl','cpf':sys.argv[1],'token':'TKN832JHGSI2937MNGHSJ9987','user_agent':'ksoap2-android/2.6.0+'}
         data = '<v:Envelope xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:d="http://www.w3.org/2001/XMLSchema" xmlns:c="http://schemas.xmlsoap.org/soap/encoding/" xmlns:v="http://schemas.xmlsoap.org/soap/envelope/">'
         data += '<v:Header /><v:Body><n0:consultarBanca id="o0" c:root="1" xmlns:n0="http://ws.habilitacao.site.servicos.getran.searchtecnologia.com.br/">'
-        data += '<cpf i:type="d:string">'+cpf+'</cpf>'
-        data += '<token i:type="d:string">TKN832JHGSI2937MNGHSJ9987</token>'
+        data += '<cpf i:type="d:string">'+configs['cpf']+'</cpf>'
+        data += '<token i:type="d:string">'+configs['token']+'</token>'
         data += '</n0:consultarBanca></v:Body></v:Envelope>'
-        headers = {'User-Agent':'ksoap2-android/2.6.0+','SOAPAction':'http://getran.detran.df.gov.br/getranServicos/','Content-Type':'text/xml;charset=utf-8'}
-        req = requests.post(url,data=data,headers=headers)
-        
-        # Regex das tags XML   
-        nome = re.search('<nome>(.*)</nome>',req.content).group(0).replace('<nome>','').replace('</nome>','')
-        renach = re.search('<renach>(.*)</renach>',req.content).group(0).replace('<renach>','').replace('</renach>','')
-        cpf = re.search('<cpf>(.*)</cpf>',req.content).group(0).replace('<cpf>','').replace('</cpf>','')
-        dataNascimento = re.search('<dataNascimento>(.*)</dataNascimento>',req.content).group(0).replace('<dataNascimento>','').replace('</dataNascimento>','')
-        local = re.search('<local>(.*)</local>',req.content).group(0).replace('<local>','').replace('</local>','')
-        cfc = re.search('<cfc>(.*)</cfc>',req.content).group(0).replace('<cfc>','').replace('</cfc>','')
-        resultado = re.search('<resultado>(.*)</resultado>',req.content).group(0).replace('<resultado>','').replace('</resultado>','')
-        data = re.search('<data>(.*)</data>',req.content).group(0).replace('<data>','').replace('</data>','')
-        hora = re.search('<hora>(.*)</hora>',req.content).group(0).replace('<hora>','').replace('</hora>','')     
-       
-        print ' Nome completo:',nome
-        print ' CPF:',cpf
-        print ' Renach:',renach
-        print ' Data de nascimento:', dataNascimento
-        print ' CFC:',cfc
-        print ' Data:',data,'Hora:',hora
-        print ' Resultado:',resultado
+        headers = {'User-Agent':configs['user_agent'],'SOAPAction':'http://getran.detran.df.gov.br/getranServicos/','Content-Type':'text/xml;charset=utf-8'}
+        req = requests.post(configs['api_getran'],data=data,headers=headers)
+        get_dados(req.content) # Chama a função get_dados e retorna os dados solicitados.
+        print ' Nome completo:',dados['nome']
+        print ' CPF:',dados['cpf']
+        print ' Renach:',dados['renach']
+        print ' Data de nascimento:', dados['dataNascimento']
+        print ' CFC:',dados['cfc']
+        print ' Data:',dados['data'],'Hora:',dados['hora']
+        print ' Resultado:',dados['resultado']
     else:
         print "[ ! ] Executar o script junto com o cpf:  ./getran.py <cpf>  | ./getran.py 1234567890"
